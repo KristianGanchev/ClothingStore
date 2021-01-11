a38c1c8a2d57940dcbb1bcdd99f13c0e16fcae82
Kristian Ganchev <kris.ganchev91@gmail.com>
2021-01-11 07:21:52 +0200

    added shop page, celection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Homepage from "./pages/homepage/homepage.component";
 import './App.css'
 import { Route, Switch } from 'react-router-dom';
+import ShopPage from "./pages/shoppage/shoppage.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
