6a34f8d0d1fa17a1688ee024386bc3a275e39eff
Kristian Ganchev <kris.ganchev91@gmail.com>
2021-01-21 13:47:11 +0200

    added backend server to handle stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,24 @@
 import StripeCheckout from 'react-stripe-checkout';
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51I96lKKK0xX483Ygqf2Lk2PvueK6LZtCV8DJN3SK83ahTms12XKj7bFc50mkmjFVcNLnPzotyTSlFDCdfAmMeIiU00yfDLTvxH';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succsesful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful');
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment! Make sure you use the provided credit cart!');
+    });
   }
 
   return(
